2e7200b27375488c8c8561d9f710dfdafb75bcd5
julianmichael <julianjohnmichael@gmail.com>
2014-07-18 07:34:30 -0700

    checkpoint commit, plane is landing

--- src/main/scala/parsing/cfg/CFGProduction.scala
+++ src/main/scala/parsing/cfg/CFGProduction.scala
@@ -11,15 +11,30 @@ case class CFGProduction[+A](
     collapseProduction(NormalTag[A](head), children).toList
   }
 
-  private[this] def collapseProduction(label: CNFTag[A], symbols: List[A]): Set[CNFProduction[A]] = {
-    symbols match {
-      case Nil                  => throw new AssertionError("Production's children are null")
-      case child :: Nil         => Set(Unary[A](NormalTag[A](head), NormalTag[A](child)))
-      case left :: right :: Nil =>
-        Set(Binary[A](label, NormalTag[A](left), NormalTag[A](right)))
-      case left :: remainder    => {
-        collapseProduction(ChunkedTag[A](remainder), remainder) + Binary[A](label, NormalTag[A](left), ChunkedTag[A](remainder))
-      }
+}
+object CFGProduction {
+  def toCNF[A](prod: CFGProduction[A]): List[CNFProduction[A]] = prod match {
+    case CFGProduction(head, children) =>
+      collapseProduction(NormalTag[A](head), children).toList
+  }
+
+  private[this] def collapseProduction(
+      label: CNFTag[A],
+      symbols: List[A]): Set[CNFProduction[A]] = symbols match {
+    case Nil                    => throw new AssertionError("Production's children are null")
+    case Empty :: Nil           => Set(ZeroEmpty[A](label))
+    case child :: Nil           => Set(Unary[A](NormalTag[A](head), NormalTag[A](child)))
+    case Empty :: Empty :: Nil  => Set(DoubleEmpty[A](label))
+    case left :: Empty :: Nil   => Set(RightEmpty[A](label, NormalTag[A](left)))
+    case Empty :: right :: Nil  => Set(LeftEmpty[A](label, NormalTag[A](right)))
+    case left :: right :: Nil   => Set(Binary[A](label, NormalTag[A](left), NormalTag[A](right)))
+    case Empty :: remainder     => {
+      collapseProduction(ChunkedTag[A](remainder), remainder)
+      + LeftEmpty[A](label, ChunkedTag[A](remainder))
+    }
+    case left :: remainder      => {
+      collapseProduction(ChunkedTag[A](remainder), remainder)
+      + Binary[A](label, NormalTag[A](left), ChunkedTag[A](remainder))
     }
   }
 }
